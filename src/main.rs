788a1b37454e597b324fe1d03bd334b48d2181be
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2021-07-19 18:20:35 +0900

    init project

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+/*
+ *
+ *  PEngine-RS Project
+ *  Copyright (C) P.Knowledge, 2016-2021 All rights reserved.
+ *  Licensed under MIT License
+ *
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
